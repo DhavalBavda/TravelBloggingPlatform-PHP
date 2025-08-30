f2c60fde66135a2bfbf6d90979e548ce7354e1e7
ArjunPraja <prajapatiarjun2801@gmail.com>
2025-08-30 10:06:23 +0530

    Database Configuration With Myql

---
+++ config/dbconfig.php
@@ -0,0 +1,37 @@
+<?php
+
+
+$host='localhost';
+$user="root";
+$pass ="";
+$dbname="Travel_Blogging_Platform";
+
+$conn=new mysqli($host,$user,$pass);
+
+
+if($conn->connect_error){
+    die("Connection Failed :".$conn->connect_error);
+
+
+}
+
+
+
+$sql="create database if not exists $dbname";
+
+if($conn->query($sql)==TRUE){
+    echo "Database '$dbname' created Successfully";
+
+}
+else{
+    echo "Error Creating Database :".$conn->error;
+
+}
+
+
+$conn->close();
+
+
+
+
+?>
